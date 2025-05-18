2fd09d800f6834724ddc5c8e5e1f0a0bb8666f6c
anandsinh01 <33709803+anandsinh01@users.noreply.github.com>
2025-05-18 00:28:51 -0400

    Remove sensitive/unneeded files and prepare for clean push

--- test.gemini.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyBPumjvfIx5J8KkSTm_K0QWz_07jxbKa-Y")
-model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
-response = model.generate_content("Hello, Gemini!")
-print(response.text)
